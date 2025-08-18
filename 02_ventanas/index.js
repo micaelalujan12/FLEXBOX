1d2a1e80d8ca6cc6ad9c48380783e832d6ab57b2
MicaUwU <micaelalujan387@gmail.com>
2025-08-18 12:32:48 -0300

    tareaa

---
+++ 02_ventanas/index.js
@@ -0,0 +1,28 @@
+
+console.log("==================================");
+console.log("Tarea 02 - Ventanas con JavaScrip")
+console.log("==================================");
+
+// Ejercicios con Ventanas
+
+// 1. Creamos una variable con una ventana para el usuario.
+let numeroUno = prompt("Introduce un número: ")
+
+// 2. Imprimo la variable en la consola.
+console.log(numeroUno);
+
+// 3. Otra variable para guardar números.
+let numeroDos = prompt("Introduce otro número: ")
+
+// 4. Imprimimos de nuevo el otro número.
+console.log(numeroDos);
+
+// Importante: convertir números a datos
+numeroUno = parseInt(numeroUno)
+numeroDos = parseInt(numeroDos)
+
+// 5. Creamos una variable para los resultados.
+let resultado = numeroUno + numeroDos;
+
+// 6. Imprimimos el resultado.
+console.log(resultado);
